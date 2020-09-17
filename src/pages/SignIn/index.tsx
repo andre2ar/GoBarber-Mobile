60d05d2ac1a33c80915d09e5f330b4431dcfa83e
André Alvim Ribeiro <andre2ar@outlook.com>
2020-09-16 21:15:07 -0300

    Sign-in

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image } from "react-native";
 
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
 import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
@@ -12,6 +15,11 @@ const SignIn: React.FC = () => {
             <Title>
                 Sign-in
             </Title>
+
+            <Input name={"email"} icon='mail' placeholder='E-mail'/>
+            <Input name={"password"} icon='lock' placeholder='Password'/>
+
+            <Button onPress={() => {console.log('pressed');}}>Sign-in</Button>
         </Container>
     )
 }
