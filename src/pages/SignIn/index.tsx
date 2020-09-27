51abe5c613f0e901c52c8ee4f2cd797f773278d9
André Alvim Ribeiro <andre2ar@outlook.com>
2020-09-27 11:06:20 -0300

    Sign-in

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,27 +1,63 @@
 import React from 'react';
-import { Image } from "react-native";
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
-import { Container, Title } from './styles';
+import {
+    Container,
+    Title,
+    ForgotPassword,
+    ForgotPasswordText,
+    CreateAccountButton,
+    CreateAccountButtonText,
+} from './styles';
 
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
     return (
-        <Container>
-            <Image source={logoImg}/>
-            <Title>
-                Sign-in
-            </Title>
+        <>
+            <ScrollView
+                keyboardShouldPersistTaps={"handled"}
+                contentContainerStyle={{flex: 1}}
+            >
+                <KeyboardAvoidingView
+                    style={{ flex: 1 }}
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'undefined'}
+                    enabled
+                >
+                    <Container>
+                        <Image source={logoImg}/>
+                        <View>
+                            <Title>
+                                Sign-in
+                            </Title>
+                        </View>
 
-            <Input name={"email"} icon='mail' placeholder='E-mail'/>
-            <Input name={"password"} icon='lock' placeholder='Password'/>
+                        <Input name={"email"} icon='mail' placeholder='E-mail'/>
+                        <Input name={"password"} icon='lock' placeholder='Password'/>
 
-            <Button onPress={() => {console.log('pressed');}}>Sign-in</Button>
-        </Container>
-    )
+                        <Button onPress={() => {console.log('pressed');}}>Sign-in</Button>
+
+                        <ForgotPassword onPress={() => {}}>
+                            <ForgotPasswordText>
+                                Forgot password
+                            </ForgotPasswordText>
+                        </ForgotPassword>
+                    </Container>
+                </KeyboardAvoidingView>
+            </ScrollView>
+
+            <CreateAccountButton onPress={() => {}}>
+                <Icon name={"log-in"} size={20} color={"#ff9000"}/>
+                <CreateAccountButtonText>
+                    Sign-up
+                </CreateAccountButtonText>
+            </CreateAccountButton>
+        </>
+    );
 }
 
 export default SignIn;
