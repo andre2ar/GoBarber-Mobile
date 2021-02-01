4e76823948f0d0806999885791d068f081abd736
André Alvim Ribeiro <andre2ar@outlook.com>
2021-01-31 21:41:30 -0300

    Book

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { View } from "react-native";
+
+import { Container } from "./styles";
 
 const AppointmentCreated: React.FC = () => {
     return (
-        <View>
-        </View>
+        <Container>
+
+        </Container>
     );
 }
 
