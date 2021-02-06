91f84a5cd6b10834773970b2ce75110f338f9e22
André Alvim Ribeiro <andre2ar@outlook.com>
2021-02-06 18:07:29 -0300

    Scheduling complete

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, {useCallback, useMemo} from "react";
+import Icon from 'react-native-vector-icons/Feather';
+import { Container, Title, Description, OkButton, OkButtonText } from "./styles";
+import {useNavigation, useRoute} from "@react-navigation/native";
+import {format} from "date-fns";
 
-import { Container } from "./styles";
+interface RouteParams {
+    date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
+    const {reset} = useNavigation();
+    const {params} = useRoute();
+    const routeParams = params as RouteParams;
+
+    const handleOkPressed = useCallback(() => {
+        reset({
+            routes: [
+                { name: 'Dashboard' }
+            ],
+            index: 0
+        })
+    }, []);
+
+    const formattedDate = useMemo(() => {
+        return format(routeParams.date, "EEEE', 'MMMM dd, yyyy' at' HH:mm'h'");
+    }, [routeParams.date]);
+
     return (
         <Container>
+            <Icon name={"check"} size={80} color={"#04d361"} />
 
+            <Title>Scheduling complete</Title>
+            <Description>{formattedDate}</Description>
+
+            <OkButton onPress={handleOkPressed}>
+                <OkButtonText>
+                    Ok
+                </OkButtonText>
+            </OkButton>
         </Container>
     );
 }
